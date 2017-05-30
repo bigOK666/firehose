3d3ccb9d3f0e1a856684ceb33fd71055ab1c6661
Lu <zeren.lu@gmail.com>
2017-05-30 20:07:37 +0200

    fulfill pythagorem function

--- double.rb
+++ double.rb
@@ -4,3 +4,14 @@ end
 
 val = double (10)
 puts val
+
+def pythagorean_theroem(a, b)
+	a_square = a * a;
+	b_square = b * b;
+	c_square = a_square + b_square;
+
+	return Math.sqrt(c_square);
+	
+end
+
+puts pythagorean_theroem(3, 4)
