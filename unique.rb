fc948799dff6aca57efdac9152aa6cefbe7fa98c
Lu <zeren.lu@gmail.com>
2017-05-30 22:39:36 +0200

    add return for function

--- unique.rb
+++ unique.rb
@@ -1,6 +1,16 @@
 array = [1, 2, 3, 4, 5, 6, 6, 4, 6, 8, 0, 9]
 output = [];
 output = array.uniq;
-puts output;
+puts output.inspect;
 
-result = [];
+def unique_element(a)
+	result = [];
+	a.each do |elem|
+		if !result.include?(elem)
+			result << elem
+		end
+
+	end
+	return result;
+end
+puts "result is #{unique_element(array)}";
