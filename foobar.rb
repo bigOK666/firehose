c9c3dbc09747d0477a9d3f48433456e851b9eca4
Lu <zeren.lu@gmail.com>
2017-05-30 23:13:05 +0200

    foobar function

--- foobar.rb
+++ foobar.rb
@@ -1,2 +1,23 @@
-puts "How many items do you want to see?"
+puts "How many items do you want to see?";
 total_item = gets.chomp.to_i;
+
+def foobar_pattern(target)
+	n = 1;
+	pattern = [];
+	while n <= target
+		if n % 3 == 0 && n % 5 !== 0
+			pattern << "Foo"
+		elsif n % 5 == 0 && n % 3 !== 0
+			pattern << "Bar"
+		elsif n % 3 == 0 && n % 5 == 0
+			pattern << "Foobar"
+		else
+			pattern << n
+		end
+	end
+
+	return pattern;
+end
+
+
+
